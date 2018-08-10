327b23f7304cbcfdd2338cc45504911d97df0b02
Rigoberto Leander Salgado Reyes <rlsalgado2006@gmail.com>
2018-08-10 10:32:35 -0500

    merging

--- libs/js/Q-move.js
+++ libs/js/Q-move.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 'use strict';
 
 const Q = new require('Q')();
@@ -12,18 +11,3 @@ exports.move = board => {
     ////en caso de no presnetar ese estado, aplicar el mov que mas valor obtenga
     
 }
-=======
-'use strict';
-
-const Q = new require('Q')();
-
-Q.load('Q.json');
-
-exports.move = board => {
-    //calcular el stado del board
-    //buscar el mayor en sus conexiones
-    //buscar de los posible mov. cuales dan ese estado(indefinido)
-    ////en caso de no presnetar ese estado, aplicar el mov que mas valor obtenga
-    
-}
->>>>>>> c35ffb33dda4c8b9a05be2f668a4ba47ce93e149
--- libs/js/R.js
+++ libs/js/R.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 'use strict';
 
 class R {
@@ -15,21 +14,3 @@ class R {
 }
 
 module.exports = R;
-=======
-'use strict';
-
-class R {
-    constructor() {
-    }
-
-    get(source, destination) {
-        const winningReward = destination === Number.MAX_SAFE_INTEGER ? 1000 : undefined;
-        const losingReward = destination === Number.MIN_SAFE_INTEGER ? -5000 : undefined;
-
-        return winningReward || losingReward || 0;
-    }
-
-}
-
-module.exports = R;
->>>>>>> c35ffb33dda4c8b9a05be2f668a4ba47ce93e149
